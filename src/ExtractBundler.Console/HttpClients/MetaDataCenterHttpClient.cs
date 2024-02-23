caa8141b46318296e3a7b198727b486979bb8a56
jvandaal <jonas.vandaal@outlook.be>
2024-02-23 09:39:28 +0100

    feat: use ACM/IDM bearer token instead of basis auth

--- src/ExtractBundler.Console/HttpClients/MetaDataCenterHttpClient.cs
+++ src/ExtractBundler.Console/HttpClients/MetaDataCenterHttpClient.cs
@@ -6,10 +6,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Security.Authentication;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using IdentityModel;
+using IdentityModel.Client;
 using Infrastructure.Configurations;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
@@ -22,6 +25,7 @@ public sealed class MetaDataCenterHttpClient
     private readonly ILogger<MetaDataCenterHttpClient>? _logger;
 
     private IDictionary<string, IEnumerable<string>> DefaultHeaders => _defaultHeaders;
+    private AccessToken? _accessToken;
 
     public MetaDataCenterHttpClient(
         IOptions<MetadataCenterOptions> options,
@@ -31,7 +35,6 @@ public sealed class MetaDataCenterHttpClient
         _options = options.Value;
         _baseUrl = new Uri(_options.BaseUrl);
         _defaultHeaders = new ConcurrentDictionary<string, IEnumerable<string>>();
-        DefaultHeaders.Add("Authorization", new string[] { $"Basic {_options.Authorization}".Trim() });
     }
 
     private ByteArrayContent GenerateCswPublicationBody(string identifier, DateTime dateStamp)
@@ -44,11 +47,11 @@ public sealed class MetaDataCenterHttpClient
 	<csw:Update>
         <csw:RecordProperty>
             <csw:Name>gmd:identificationInfo/gmd:MD_DataIdentification/gmd:citation/gmd:CI_Citation/gmd:date/gmd:CI_Date/gmd:date/gco:Date[../../gmd:dateType/gmd:CI_DateTypeCode/@codeListValue=""publication""]</csw:Name>
-            <csw:Value>{dateStamp.ToString("yyyy-MM-dd")}</csw:Value>
+            <csw:Value>{dateStamp.ToString("O")}</csw:Value>
         </csw:RecordProperty>
         <csw:RecordProperty>
             <csw:Name>gmd:dateStamp/gco:Date</csw:Name>
-            <csw:Value>{dateStamp.ToString("yyyy-MM-dd")}</csw:Value>
+            <csw:Value>{dateStamp.ToString("O")}</csw:Value>
         </csw:RecordProperty>
 		<csw:Constraint version=""1.1.0"">
 			<ogc:Filter>
@@ -93,11 +96,13 @@ public sealed class MetaDataCenterHttpClient
     private async Task<string?> GetXsrfToken(CancellationToken cancellationToken = default)
     {
         var requestMessage =
-            new HttpRequestMessage(HttpMethod.Post, new Uri(_baseUrl, "/metadatacenter/srv/dut/info?info=me"));
+            new HttpRequestMessage(HttpMethod.Get, new Uri(_baseUrl, "/srv/eng/info?type=me"));
         foreach (var header in DefaultHeaders)
         {
             requestMessage.Headers.TryAddWithoutValidation(header.Key, header.Value);
         }
+
+        requestMessage.Headers.TryAddWithoutValidation("Authorization", $"Bearer {GetAccessToken()}");
 
         string? xsrfToken = null;
         try
@@ -139,11 +144,13 @@ public sealed class MetaDataCenterHttpClient
         }
 
         var requestMessage =
-            new HttpRequestMessage(HttpMethod.Post, new Uri(_baseUrl, "/metadatacenter/srv/dut/csw-publication"));
+            new HttpRequestMessage(HttpMethod.Post, new Uri(_baseUrl, "/srv/dut/csw-publication"));
         foreach (var header in DefaultHeaders)
         {
             requestMessage.Headers.TryAddWithoutValidation(header.Key, header.Value);
         }
+
+        requestMessage.Headers.TryAddWithoutValidation("Authorization", $"Bearer {GetAccessToken()}");
 
         requestMessage.Headers.TryAddWithoutValidation("X-XSRF-TOKEN", xsrfToken);
 
@@ -168,7 +175,7 @@ public sealed class MetaDataCenterHttpClient
             }
             break;
         }
-       
+
         var xmlResponseContent = await response.Content.ReadAsStreamAsync(cancellationToken);
         var xmlResponse = await XDocument.LoadAsync(xmlResponseContent, LoadOptions.None, cancellationToken);
         return xmlResponse;
@@ -178,11 +185,13 @@ public sealed class MetaDataCenterHttpClient
     {
         var requestMessage =
             new HttpRequestMessage(HttpMethod.Get,
-                new Uri(_baseUrl, $"/metadatacenter/srv/api/records/{GetIdentifierValue(identifier)}/formatters/xml"));
+                new Uri(_baseUrl, $"/srv/api/records/{GetIdentifierValue(identifier)}/formatters/xml"));
         foreach (var header in DefaultHeaders)
         {
             requestMessage.Headers.TryAddWithoutValidation(header.Key, header.Value);
         }
+
+        requestMessage.Headers.TryAddWithoutValidation("Authorization", $"Bearer {GetAccessToken()}");
 
         using var httpClient = new HttpClient(new HttpClientHandler() { UseCookies = false });
         var response = await httpClient.SendAsync(requestMessage, cancellationToken).ConfigureAwait(false);
@@ -202,11 +211,13 @@ public sealed class MetaDataCenterHttpClient
                 HttpMethod.Get,
                 new Uri(
                     _baseUrl,
-                    $"/metadatacenter/srv/api/records/{GetIdentifierValue(identifier)}/formatters/xsl-view-default?output=pdf&language=dut&approved=true"));
+                    $"/srv/api/records/{GetIdentifierValue(identifier)}/formatters/xsl-view?output=pdf&language=dut&attachment=true"));
         foreach (var header in DefaultHeaders)
         {
             requestMessage.Headers.TryAddWithoutValidation(header.Key, header.Value);
         }
+
+        requestMessage.Headers.TryAddWithoutValidation("Authorization", $"Bearer {GetAccessToken()}");
 
         using var httpClient = new HttpClient(new HttpClientHandler { UseCookies = false });
         var response = await httpClient.SendAsync(requestMessage, cancellationToken).ConfigureAwait(false);
@@ -218,5 +229,50 @@ public sealed class MetaDataCenterHttpClient
 
         var pdf = await response.Content.ReadAsByteArrayAsync(cancellationToken);
         return pdf;
+    }
+
+    private async Task<string> GetAccessToken()
+    {
+        if (_accessToken is not null && !_accessToken.IsExpired)
+        {
+            return _accessToken.Token;
+        }
+
+        var tokenClient = new TokenClient(
+            () => new HttpClient(),
+            new TokenClientOptions
+            {
+                Address = _options.TokenEndPoint,
+                ClientId = _options.ClientId,
+                ClientSecret = _options.ClientSecret,
+                Parameters = new Parameters(new[] { new KeyValuePair<string, string>("scope", "vo_info dv_metadata_read dv_metadata_write") })
+            });
+
+        var response = await tokenClient.RequestTokenAsync(OidcConstants.GrantTypes.ClientCredentials);
+
+        if (response.IsError)
+        {
+            throw new AuthenticationException(response.ErrorDescription);
+        }
+
+        _accessToken = new AccessToken(response.AccessToken!, response.ExpiresIn);
+
+        return _accessToken.Token;
+    }
+}
+
+public class AccessToken
+{
+    private readonly DateTime _expiresAt;
+
+    public string Token { get; }
+
+    // Let's regard it as expired 10 seconds before it actually expires.
+    public bool IsExpired => _expiresAt < DateTime.Now.Add(TimeSpan.FromSeconds(10));
+
+    public AccessToken(string token, int expiresIn)
+    {
+        _expiresAt = DateTime.Now.AddSeconds(expiresIn);
+        Token = token;
     }
 }
